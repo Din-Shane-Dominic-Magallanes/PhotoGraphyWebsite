725a77af8578fc1f2c1d66b6506645df2f0f13f7
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2025-03-15 22:01:57 +0800

    Another useless commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import Typography from '@mui/material/Typography';
+import { Button } from "@mui/material";
+import Grid from "./components/Grid.jsx";
+import Card from "./components/Card.jsx";
 
 const App = () => {
     return (
-        <div>
-            <Typography variant="h1">Hello, World!</Typography>
-        </div>
+        <>
+            <div>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </div>
+            <Grid />
+            <Card />
+        </>    
     )
 }
 
